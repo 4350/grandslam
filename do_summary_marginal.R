58bfe14ddcf5190bbf235093d48f7f56e3120f09
Gustaf Soldan <gustafsoldan@gmail.com>
2016-09-22 18:51:12 +0200

    boom first

---
+++ do_summary_marginal.R
@@ -0,0 +1,36 @@
+#' Create marginal summary statistics table and plots
+
+# Libraries ----
+library(dplyr)
+library(fBasics)
+library(tidyr)
+library(ggplot2)
+library(scales)
+library(ggfortify)
+library(gridExtra)
+library(devtools)
+
+# Reset workspace and load return data ----
+rm(list = ls())
+load('data/derived/weekly-estim.RData')
+load('data/derived/weekly-full.Rdata')
+
+# Load functions ----
+load_all('wimbledon')
+
+# Descriptive statistics tables ---
+summaryTable(df, 'Full')
+summaryTable(df.estim, 'Estim')
+
+# Marginal plots ----
+varlist = list('Mkt.RF','HML','SMB','Mom','RMW','CMA')
+
+lapply(varlist,
+       function(varlist) statPlots(df, 'Full', varlist))
+
+lapply(varlist,
+       function(varlist) statPlots(df.estim, 'Estim', varlist))
+
+# Cumulative plots
+cumretPlots(df, 'Full')
+cumretPlots(df.estim, 'Estim')
