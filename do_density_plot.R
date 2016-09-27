f29cbd241459ad3949b93074af161ae3e9c15901
Gustaf Soldan <gustafsoldan@gmail.com>
2016-09-27 18:03:47 +0200

    Change density plot to include 99.95% normal confidence ellipse. Further motivation of non-gaussian stdres

--- do_density_plot.R
+++ do_density_plot.R
@@ -5,10 +5,17 @@
 library(ggplot2)
 library(GGally)
 library(dplyr)
+library(ellipse)
+library(mvtnorm)
+
 
 # Reset workspace and load residuals from GARCH ----
 rm(list = ls())
 load('data/derived/garch_stdres.RData')
+
+# Set colors and seed
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+set.seed(17)
 
 # Do and save density plots ----
 jpeg(filename = "output/densityGARCHresiduals.jpeg", height = 8.3, width = 11.7, units = 'in', res = 300)
@@ -19,7 +26,14 @@ g <- df.stdres %>% select(-Date) %>%
         geom_density2d(...)
       }
     ),
-    diag = 'blank'
+    diag = 'blank',
+    upper = list(
+      continuous = function(data, mapping, ...) {
+        ggplot(data = data, mapping = mapping)+
+          geom_bin2d(..., bins = 45)+
+          stat_ellipse(..., type = 'norm', level = 0.9995, color = cbbPalette[2])
+      }
+    )
 )
 print(g)
 dev.off()
