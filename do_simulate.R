bafa77927fd44c1c9404f84c2c60d27b113c3838
Victor Andrée <hello@victorandree.se>
2016-09-30 17:09:49 +0200

    Working simulation code!!!

--- do_simulate.R
+++ do_simulate.R
@@ -50,276 +50,76 @@
 library(devtools)
 library(rugarch)
 library(tictoc)
+library(uuid)
+library(parallel)
 load_all('wimbledon')
-rm(list = ls())
 
 # SETUP ------------------------------------------------------------------
+rm(list = ls())
+
 load('data/derived/model_GARCH.RData')
 load('data/derived/model_copula_dynamic_ghskt.RData')
 
+# These are the models that will be used for simulation. The format is
+# ad hoc for the copula model, but matches output from estimation.
+#
+# The GARCH models should be a list of uGARCHfit objects (ugarchspec doesn't
+# work because of reasons)
 kCopulaModel <- model.copula.dynamic.ghskt
 kGARCHModels <- model.GARCH
+kOutputPath <- 'data/derived/simulation/ghskt'
+
+# Number of simulations per run
+T <- 4500
+
+# Parallel simulations: Give number of simulations to run at the same time
+B <- parallel::detectCores() - 1
+
 rm(model.GARCH, model.copula.dynamic.ghskt)
-
-T <- 5000
-N <- ncol(kCopulaModel$Omega)
-
-# Build copula univariate distributions
-sim.c.uv.dists <- function(p.c) {
-  p.d <- p.c$dist.params
-  
-  dist.gauss <- function(i) gauss()
-  dist.ght   <- function(i) student.t(nu = p.d$df)
-  dist.ghskt <- function(i) student.t(nu = p.d$df, gamma = p.d$skew[[i]])
-  
-  fn <- dist.gauss
-  if (!is.null(p.d$df)) {
-    if (!is.null(p.d$skew)) {
-      fn <- dist.ghskt
-    } else {
-      fn <- dist.ght
-    }
-  }
-  
-  lapply(seq(ncol(p.c$Omega)), fn)
-}
-
-sim.c.rghyp <- function(p.c, Correlation) {
-  p.d <- p.c$dist.params
-  N <- ncol(p.c$Omega)
-  mu <- rep(0, N)
-  
-  mv.dist <- NULL
-  if (is.null(p.d$df)) {
-    mv.dist <- gauss(
-      mu = mu,
-      sigma = Correlation
-    )
-  }
-  else {
-    skew <- if (is.null(p.d$skew)) rep(0, N) else p.d$skew
-    
-    mv.dist <- student.t(
-      nu = p.d$df,
-      mu = mu,
-      gamma = skew,
-      sigma = Correlation
-    )
-  }
-  
-  ghyp::rghyp(1, mv.dist)
-}
-
-sim.c.Q_tp1 <- function(p.c, Q_t, shocks.std_t) {
-  (1 - p.c$alpha - p.c$beta) * p.c$Omega +
-    p.c$beta * Q_t +
-    p.c$alpha * (shocks.std_t %*% t(shocks.std_t))
-}
-
-# SIMULATION SETUP -------------------------------------------------------
-
-# Create univariate distribution objects depending on copula parameters
-c.uv.dists <- sim.c.uv.dists(kCopulaModel)
-
-# We intialize the correlation to (the correlationalized version of) Omega
-Q <- array(dim = c(N, N, T))
-Correlation <- NA * Q
-Q[,, 1] <- kCopulaModel$Omega
-Correlation[,, 1] <- dc.Correlation(array(Q[,, 1], dim = c(N, N, 1)))
-
-shocks <- matrix(ncol = N, nrow = T)
-shocks.std <- shocks * NA
 
 # COPULA SIMULATION LOOP -------------------------------------------------
 
-tic()
-for (t in seq(T)) {
-  # Get shocks from MV distribution for this period
-  shocks[t, ] <- sim.c.rghyp(kCopulaModel, Correlation[,, t])
-  shocks.std[t, ] <- dc.shocks.std(
-    rbind(shocks[t, ]),
-    c.uv.dists,
-    alpha = kCopulaModel$alpha,
-    beta = kCopulaModel$beta
-  )
+dir.create(kOutputPath, recursive = T, showWarnings = F)
+
+cluster <- makeCluster(B)
+clusterEvalQ(cluster, library(devtools))
+clusterEvalQ(cluster, library(ghyp))
+clusterEvalQ(cluster, library(rugarch))
+clusterEvalQ(cluster, load_all('wimbledon'))
+
+simulate <- function(i, T, garch, copula) {
+  # Simulate
+  result <- sim.c(T, garch, copula)
   
-  # Prepare Q and Correlation for next period
-  if (t < T) {
-    Q[,, t + 1] <- sim.c.Q_tp1(
-      kCopulaModel,
-      Q_t = Q[,, t],
-      shocks.std_t = shocks.std[t, ]
-    )
-    
-    Correlation[,, t + 1] <- dc.Correlation(
-      array(Q[,, t + 1], dim = c(N, N, 1))
-    )
-  }
+  # Pretty results
+  data.frame(
+    series = result$series,
+    sigma = result$sigma,
+    resid = result$resid
+  )
 }
-toc()
-rm(t)
 
-# ARMA-GARCH -------------------------------------------------------------
-
-tic()
-garchsim <- sapply(seq(N), function(i) {
-  # Compute uniform residuals using the marginal distribution of the copula
-  u <- ghyp::pghyp(shocks[, i], c.uv.dists[[i]])
-  
-  # Compute GARCH shocks from the uniforms using the parametrization of the
-  # GARCH models. Note that rugarch uses the GHSKT distribution, however, in
-  # a different parametrization to ghyp. However, we want to use ghyp for
-  # performance reasons hence the funky function here
-  
-  # XXX This should use the fit if using a fit!!!
-  model <- kGARCHModels[[i]]
-  pars <- model@model$pars[, 'Level']
-  stdres <- garch.qghyp.rugarch(
-    u,
-    skew = pars['skew'],
-    shape = pars['shape']
+n.simulations = 0
+while (TRUE) {
+  tic()
+  results <- parLapply(
+    cluster,
+    seq(B),
+    simulate,
+    T = T,
+    garch = kGARCHModels,
+    copula = kCopulaModel
   )
+  toc()
   
-  # ugarchpath if you have specs
-  ugarchsim(
-    model,
-    n.sim = T,
-    m.sim = 1,
-    custom.dist = list(name = 'sample', distfit = cbind(stdres))
-  )
-})
-toc()
+  # Save to unique file
+  lapply(results, function(result) {
+    file <- file.path(kOutputPath, paste0(UUIDgenerate(), '.csv'))
+    write.csv(result, file = file)
+  })
+  
+  n.simulations <- n.simulations + length(results)
+  cat('Total: ', n.simulations, '\n')
+}
 
-# SAVE OUTPUT ------------------------------------------------------------
-
-output <- list(
-  series = sapply(garchsim, function(p) t(p@simulation$seriesSim)),
-  sigma = sapply(garchsim, function(p) t(p@simulation$sigmaSim)),
-  resid = sapply(garchsim, function(p) t(p@simulation$residSim))
-)
-
-# Hard-Coded Demo Models -------------------------------------------------
-
-# params.copula <- list(
-#   dist.params = list(
-#     df = 8,
-#     skew = c(-0.25, 0.25, 0.10, 0.05, 0.10, -0.05)
-#   ),
-#   alpha = 0.06,
-#   beta = 0.91,
-#   Omega = matrix(c(
-#     1.00, 0.50, 0.50, 0.50, 0.50, 0.50,
-#     0.50, 1.00, 0.50, 0.50, 0.50, 0.50,
-#     0.50, 0.50, 1.00, 0.50, 0.50, 0.50,
-#     0.50, 0.50, 0.50, 1.00, 0.50, 0.50,
-#     0.50, 0.50, 0.50, 0.50, 1.00, 0.50,
-#     0.50, 0.50, 0.50, 0.50, 0.50, 1.00
-#   ), ncol = 6, byrow = T)
-# )
-# kGARCHModels <- list(
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(2, 0)),
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     distribution.model = 'ghst',
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       ar1 = 0.75,
-#       ar2 = 0.20,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   ),
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(0, 0)),
-#     distribution.model = 'ghst',
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   ),
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(0, 0)),
-#     distribution.model = 'ghst',
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   ),
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(0, 0)),
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     distribution.model = 'ghst',
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   ),
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(0, 0)),
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     distribution.model = 'ghst',
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   ),
-#   ugarchspec(
-#     mean.model = list(armaOrder = c(0, 0)),
-#     variance.model = list(
-#       model = 'fGARCH',
-#       submodel = 'GJRGARCH'
-#     ),
-#     distribution.model = 'ghst',
-#     fixed.pars = list(
-#       mu = 0.0011691388,
-#       omega = 0.0000127421,
-#       alpha1 = 0.0851367486,
-#       beta1 = 0.8680192797,
-#       eta11 = 0.3914889074,
-#       skew = -2.5203249430,
-#       shape = 12.9541169366
-#     )
-#   )
-# )
-
+stopCluster(cluster)
---
+++ wimbledon/R/simulate.R
@@ -0,0 +1,148 @@
+#' One-step ahead Q matrix
+#'
+#' @param p.c copula model
+#' @param Q_t Q this period
+#' @param shocks.std_t shocks this period
+#'
+#' @return Q next period
+#' @export
+sim.c.Q_tp1 <- function(p.c, Q_t, shocks.std_t) {
+  (1 - p.c$alpha - p.c$beta) * p.c$Omega +
+    p.c$beta * Q_t +
+    p.c$alpha * (shocks.std_t %*% t(shocks.std_t))
+}
+
+#' Simulate copula shocks (default 1 period)
+#'
+#' @param p.c copula parameters
+#' @param Correlation Correlation matrix (Sigma)
+#' @param n number of shocks (for constant Correlation)
+#'
+#' @return Nxn shocks
+#' @export
+sim.c.rghyp <- function(p.c, Correlation, n = 1) {
+  p.d <- p.c$dist.params
+  N <- ncol(p.c$Omega)
+  mu <- rep(0, N)
+  
+  mv.dist <- NULL
+  if (is.null(p.d$df)) {
+    mv.dist <- gauss(
+      mu = mu,
+      sigma = Correlation
+    )
+  }
+  else {
+    skew <- if (is.null(p.d$skew)) rep(0, N) else p.d$skew
+    
+    mv.dist <- student.t(
+      nu = p.d$df,
+      mu = mu,
+      gamma = skew,
+      sigma = Correlation
+    )
+  }
+  
+  ghyp::rghyp(n, mv.dist)
+}
+
+#' Simulate NxT shocks from Copula model
+#'
+#' @param T number of periods to simulate
+#' @param copula parameters describing copula model
+#'
+#' @return
+#' @export
+sim.c.copula <- function(T, copula) {
+  # Setup
+  N <- ncol(copula$Omega)
+  
+  # Univariate distributions for shocks
+  c.uv.dists <- dc.uv.dists(N, copula$dist.params)
+  
+  # Copula model results
+  shocks <- matrix(ncol = N, nrow = T)
+  Q <- array(dim = c(N, N, T))
+  
+  shocks.std <- shocks * NA
+  Correlation <- NA * Q
+  
+  # We intialize the correlation to (the correlationalized version of) Omega
+  Q[,, 1] <- copula$Omega
+  Correlation[,, 1] <- dc.Correlation(array(Q[,, 1], dim = c(N, N, 1)))
+  
+  for (t in seq(T)) {
+    # Get shocks from MV distribution for this period
+    shocks[t, ] <- sim.c.rghyp(copula, Correlation[,, t])
+    
+    shocks.std[t, ] <- dc.shocks.std(
+      rbind(shocks[t, ]),
+      c.uv.dists,
+      alpha = copula$alpha,
+      beta = copula$beta
+    )
+    
+    # Prepare Q and Correlation for next period
+    if (t < T) {
+      Q[,, t + 1] <- sim.c.Q_tp1(
+        copula,
+        Q_t = Q[,, t],
+        shocks.std_t = shocks.std[t, ]
+      )
+      
+      Correlation[,, t + 1] <- dc.Correlation(
+        array(Q[,, t + 1], dim = c(N, N, 1))
+      )
+    }
+  }
+  
+  # Compute uniform residuals using the marginal distribution of the copula
+  sapply(seq(N), function(i) ghyp::pghyp(shocks[, i], c.uv.dists[[i]]))
+}
+
+#' Simulate GARCH paths from uniforms (given by copula)
+#'
+#' @param u NxT uniform residuals
+#' @param garch list of N uGARCHfit models
+#'
+#' @return list of series, sigma and residuals matrices
+#' @export
+sim.c.GARCH <- function(u, garch) {
+  T <- nrow(u)
+  
+  sim <- sapply(seq(ncol(u)), function(i) {
+    model <- garch[[i]]
+    pars <- model@model$pars[, 'Level']
+    
+    stdres <- garch.qghyp.rugarch(
+      u[, i],
+      skew = pars['skew'],
+      shape = pars['shape']
+    )
+    
+    ugarchsim(
+      model,
+      n.sim = T,
+      m.sim = 1,
+      custom.dist = list(name = 'sample', distfit = cbind(stdres))
+    )
+  })
+  
+  # Collect it all nicely (we don't care about the models or the seed)
+  list(
+    series = sapply(sim, function(p) t(p@simulation$seriesSim)),
+    sigma = sapply(sim, function(p) t(p@simulation$sigmaSim)),
+    resid = sapply(sim, function(p) t(p@simulation$residSim))
+  )
+}
+
+#' Simulate a full series of T observation from GARCH and Copula
+#'
+#' @param garch List of GARCH models (uGARCHfit objects)
+#' @param copula Copula parametrization
+#'
+#' @return
+#' @export
+sim.c <- function(T, garch, copula) {
+  sim.c.GARCH(sim.c.copula(T, copula), garch)
+}
