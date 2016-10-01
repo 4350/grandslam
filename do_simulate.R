54392dfa008a798fc789edf2e67cd7c25b41e1c9
Victor Andrée <hello@victorandree.se>
2016-10-01 10:58:13 +0200

    Simulate using LB (dunno how it matters but)

--- do_simulate.R
+++ do_simulate.R
@@ -1,33 +1,33 @@
 #' Simulate Return Series from ARMA-GARCH-DCC Copula Model
-#' 
+#'
 #' For each T,
-#' 
+#'
 #' Generate a battery of shocks from copula MV distribution using
 #' conditional correlation matrix (for t = 0, use unconditional)
-#' 
+#'
 #' Then, to update correlation matrix for next period:
-#' 
+#'
 #' 1. Standardize shocks
 #' 2. Send through DCC model to update correlation matrix for next period
-#' 
+#'
 #' To generate return series:
-#' 
+#'
 #' 1. Perform uniform transformation of (non-standardized) shock according to
 #'    copula univariate distributions
 #' 2. Compute quantile according to each GARCH model to get GARCH shocks
 #' 3. Send through ARMA-GARCH model to get return for this period
-#' 
+#'
 #' Nothing in the copula depends on the GARCH steps; it makes sense to generate
 #' the full copula series of shocks first, and then transform these shocks into
 #' returns.
-#' 
+#'
 #' The copula simulation is hard to run in parallel, as it involves
 #' multivariate recursion.
-#' 
+#'
 #' In order to simulate, the models must be fully specified.
-#' 
+#'
 #' For the copula, I need this:
-#' 
+#'
 #'      list(
 #'        dist.params = list(
 #'          df = 10,             # NULL if normal
@@ -42,7 +42,7 @@
 #'          0.50, 0.50, 0.50, 1.00
 #'        ), nrow = 4, byrow = T)
 #'      )
-#'  
+#'
 #'  For each ARMA-GARCH, I need the spec and parameters. This should work
 #'  fine with whatever input/output rugarch deals with.
 
@@ -90,7 +90,7 @@ clusterEvalQ(cluster, load_all('wimbledon'))
 simulate <- function(i, T, garch, copula) {
   # Simulate
   result <- sim.c(T, garch, copula)
-  
+
   # Pretty results
   data.frame(
     series = result$series,
@@ -102,7 +102,7 @@ simulate <- function(i, T, garch, copula) {
 n.simulations = 0
 while (TRUE) {
   tic()
-  results <- parLapply(
+  results <- parLapplyLB(
     cluster,
     seq(B),
     simulate,
@@ -111,13 +111,13 @@ while (TRUE) {
     copula = kCopulaModel
   )
   toc()
-  
+
   # Save to unique file
   lapply(results, function(result) {
     file <- file.path(kOutputPath, paste0(UUIDgenerate(), '.csv'))
     write.csv(result, file = file)
   })
-  
+
   n.simulations <- n.simulations + length(results)
   cat('Total: ', n.simulations, '\n')
 }
