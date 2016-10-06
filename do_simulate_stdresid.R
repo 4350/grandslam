4f3bce457837e7b3c527d67577f1a637f652e1a4
Victor Andrée <hello@victorandree.se>
2016-10-06 16:48:05 +0200

    Code to simulate standardized residuals against filtered data

---
+++ do_simulate_stdresid.R
@@ -0,0 +1,51 @@
+#' Simulate 1-step residuals from copula
+
+
+# Setup ------------------------------------------------------------------
+
+library(devtools)
+library(tictoc)
+load_all('wimbledon')
+
+rm(list = ls())
+
+# First index for which we simulate distribution t + 1
+kTStart <- 2479
+kTEnd <- 2765
+kNSim <- 1e5
+kModelName <- 'dynamic_ghskt'
+
+output.directory <- file.path('data/derived/stdresid', kModelName)
+
+load(paste0('data/derived/filtered_', kModelName, '.RData'))
+
+dir.create(output.directory, recursive = T, showWarnings = F)
+
+# Test -------------------------------------------------------------------
+
+cluster <- prepare.cluster()
+
+uv <- filtered.series$filtered$copula$uv.dists
+garch <- filtered.series$garch
+
+for (t in kTStart:kTEnd) {
+  # Shocks for the next period are generated with next period's conditional
+  # correlation matrix
+  
+  tic(paste(kNSim, "simulated for t =", t + 1))
+  Correlation_tp1 <- filtered.series$filtered$copula$Correlation[,, t + 1]
+  
+  mv.dist <- dc.mv.dist(
+    filtered.series$copula$params$dist.params,
+    Correlation_tp1
+  )
+  
+  stdresid <- shocks2stdresid(rghyp(kNSim, mv.dist), uv, garch, cluster)
+  write.csv(
+    stdresid,
+    file.path(output.directory, paste0(kNSim, "_", t + 1, '.csv'))
+  )
+  toc()
+}
+
+stopCluster(cluster)
