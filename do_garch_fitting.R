4208b552e27c122f40dcb8aaf856e75c69b599f3
Gustaf Soldan <gustafsoldan@gmail.com>
2016-11-01 15:40:24 +0100

    now calls model instead of submodel, saves to another name, might cause issue later

--- do_garch_fitting.R
+++ do_garch_fitting.R
@@ -15,34 +15,34 @@ rm(list = ls())
 load('data/derived/weekly-estim.RData')
 
 #' Function to do GARCH fit and save to different objects
-#' depending on the submodel (if GJR) and dist (norm,
+#' depending on the model (gjrGARCH or sGARCH) and dist (norm,
 #' std, ghst)
 #' 
 #' @param df.estim 
-#' @param submodel either GJRGARCH or GARCH
+#' @param model either gjrGARCH or sGARCH
 #' @param dist either norm, std or ghst
 #' 
 #' @return saves the garch_fits objects in derived data
-do_garch_fit <- function(df.estim, submodel, dist, nOOS = 0) {
+do_garch_fit <- function(df.estim, model, dist, nOOS = 0) {
   
   # Estimate GARCH specifications ----
   speclist <- list(
-    ARMA00 = garch.specgen(0,0, submodel = submodel, dist = dist),
-    ARMA10 = garch.specgen(1,0, submodel = submodel, dist = dist),
-    ARMA01 = garch.specgen(0,1, submodel = submodel, dist = dist),
-    ARMA11 = garch.specgen(1,1, submodel = submodel, dist = dist),
-    ARMA20 = garch.specgen(2,0, submodel = submodel, dist = dist),
-    ARMA21 = garch.specgen(2,1, submodel = submodel, dist = dist),
-    ARMA22 = garch.specgen(2,2, submodel = submodel, dist = dist),
-    ARMA02 = garch.specgen(0,2, submodel = submodel, dist = dist),
-    ARMA12 = garch.specgen(1,2, submodel = submodel, dist = dist),
-    ARMA30 = garch.specgen(3,0, submodel = submodel, dist = dist),
-    ARMA31 = garch.specgen(3,1, submodel = submodel, dist = dist),
-    ARMA32 = garch.specgen(3,2, submodel = submodel, dist = dist),
-    ARMA33 = garch.specgen(3,3, submodel = submodel, dist = dist),
-    ARMA03 = garch.specgen(0,3, submodel = submodel, dist = dist),
-    ARMA13 = garch.specgen(1,3, submodel = submodel, dist = dist),
-    ARMA23 = garch.specgen(2,3, submodel = submodel, dist = dist)
+    ARMA00 = garch.specgen(0,0, model = model, dist = dist),
+    ARMA10 = garch.specgen(1,0, model = model, dist = dist),
+    ARMA01 = garch.specgen(0,1, model = model, dist = dist),
+    ARMA11 = garch.specgen(1,1, model = model, dist = dist),
+    ARMA20 = garch.specgen(2,0, model = model, dist = dist),
+    ARMA21 = garch.specgen(2,1, model = model, dist = dist),
+    ARMA22 = garch.specgen(2,2, model = model, dist = dist),
+    ARMA02 = garch.specgen(0,2, model = model, dist = dist),
+    ARMA12 = garch.specgen(1,2, model = model, dist = dist),
+    ARMA30 = garch.specgen(3,0, model = model, dist = dist),
+    ARMA31 = garch.specgen(3,1, model = model, dist = dist),
+    ARMA32 = garch.specgen(3,2, model = model, dist = dist),
+    ARMA33 = garch.specgen(3,3, model = model, dist = dist),
+    ARMA03 = garch.specgen(0,3, model = model, dist = dist),
+    ARMA13 = garch.specgen(1,3, model = model, dist = dist),
+    ARMA23 = garch.specgen(2,3, model = model, dist = dist)
   )
   
   # Parallelize the estimation of GARCH, by splitting up the estimation of each
@@ -68,9 +68,9 @@ do_garch_fit <- function(df.estim, submodel, dist, nOOS = 0) {
   stopCluster(cluster)
   rm(cluster)  
   if(nOOS != 0) {
-    save(fits, file = sprintf('data/derived/garch/oos_garch_fits_%s_%s.RData', submodel, dist))
+    save(fits, file = sprintf('data/derived/garch/oos_garch_fits_%s_%s.RData', model, dist))
   } else {
-    save(fits, file = sprintf('data/derived/garch/garch_fits_%s_%s.RData', submodel, dist))
+    save(fits, file = sprintf('data/derived/garch/garch_fits_%s_%s.RData', model, dist))
   }
 }
 
