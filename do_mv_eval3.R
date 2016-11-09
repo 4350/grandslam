b66aec415c93c785fc720055ef637d9792cce4ea
Victor Andrée <hello@victorandree.se>
2016-11-09 17:49:37 +0100

    mv eval 3

---
+++ do_mv_eval3.R
@@ -0,0 +1,43 @@
+rm(list = ls())
+
+library(dplyr)
+
+MODEL_NAME <- 'results_full_dynamic_std_10000'
+# MODEL_NAME <- 'results_full_sample'
+
+STRATEGIES <- list(
+  '5F',
+  '5F_EXCL_HML',
+  '5F_EXCL_CMA',
+  '6F',
+  '6F_EXCL_HML',
+  '6F_EXCL_CMA'
+)
+
+load_field <- function(field) {
+  out <- lapply(STRATEGIES, function(strategy) {
+    load(sprintf('data/derived/mv/%s_%s.RData', MODEL_NAME, strategy))
+    results[[field]]
+  })
+  
+  names(out) <- STRATEGIES
+  out
+}
+
+# Summary statistics of realized returns ----
+returns <- load_field('portfolio_return')
+
+stats <- lapply(returns, function(strategy) {
+  stats_labels <- c('Mean', 'Stdev', 'Skewness', 'Kurtosis')
+  stats <- as.list(fBasics::basicStats(strategy)[stats_labels, ])
+  names(stats) <- stats_labels
+  stats$Mean <- 52 * stats$Mean
+  stats$Stdev <- sqrt(52) * stats$Stdev
+  
+  # SR
+  SR <- stats$Mean / (stats$Stdev)
+  data.frame(stats, SR = SR)
+})
+
+cat(sprintf('== %s ==\n\n', MODEL_NAME))
+print(bind_rows(stats, .id = 'Strategy'))
