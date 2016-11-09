7e61ae654ca300f4a7325046117d5b7080a20c59
Victor Andrée <hello@victorandree.se>
2016-11-09 17:49:12 +0100

    eval mv

--- do_mv_eval.R
+++ do_mv_eval.R
@@ -1,5 +1,7 @@
+# XXX ----
 library(ggplot2)
 library(tidyr)
+library(zoo)
 
 get_portfolio_field <- function(field, files) {
   lapply(files, function(file) {
@@ -9,31 +11,54 @@ get_portfolio_field <- function(field, files) {
 }
 
 weights <- get_portfolio_field('weights', list(
-  'constant_norm_100000_All',
-  'indep_100000_All'
+  'dynamic_ghst_10000_5F',
+  'dynamic_ghst_10000_5F_EXCL_HML'
 ))
 
-weights <- data.frame(
-  t = 1:914,
-  sapply(weights, function(w) w[, 1])
-)
+weights_df <- lapply(weights, function(model) {
+  model <- apply(model, 2, function(w) rollmeanr(w, 26))
+  
+  model <- data.frame(
+    t = 1:nrow(model),
+    model
+  )
+  gather(model, 'Factor', 'Weights', -1)
+})
+names(weights_df) <- c('All', 'Excl. HML')
 
-gathered <- gather(weights, Strategy, Market, -1)
+weights_df <- bind_rows(weights_df, .id = 'Strategy')
+# weights_df <- lapply(weights,
+#                      function(model) {
+#                        data.frame(model) %>%
+#                           gather(., 'Factor', 'Weights') %>%
+#                           group_by(Factor) %>%
+#                           rollmeanr(., 52)
+#                      })
 
-ggplot(gathered, aes(x = t, y = Market, color = Strategy)) +
+
+
+# 
+# weights_df <- data.frame(
+#   t = 1:(914 - 51),
+#   HML_ALL = rollmeanr(weights[[1]][, 'HML'], 52),
+#   HML = rollmeanr(weights[[2]][, 'HML'], 52)
+# )
+
+# gathered <- gather(weights_df, 'Strategy', 'Weights', -1)
+
+ggplot(weights_df, aes(x = t, y = Weights, color = Strategy)) +
+  facet_grid(. ~ Factor) +
   geom_line()
+# ggplot(gathered, aes(x = t, y = Weights, color = Strategy)) +
+#   geom_line()
+
 
 # Returns ----------------------------------------------------------------
 
 returns <- data.frame(
   t = 1:914,
   portfolio_returns(list(
-    'dynamic_std_10000_All',
-    'dynamic_norm_10000_All',
-    'constant_std_10000_All',
-    'constant_ghst_10000_All',
-    'constant_norm_10000_All',
-    'indep_100000_All'
+    
   ))
 )
 
