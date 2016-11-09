40967e4c77f8673f2c55d5d126e0f28b42965ccb
Gustaf Soldan <gustafsoldan@gmail.com>
2016-11-09 16:13:07 +0100

    rsq added to output

--- do_ff_regressions.R
+++ do_ff_regressions.R
@@ -32,6 +32,15 @@ ff_reg <- function(y, x, df) {
   coeftest(reg_fit, vcov = sandwich)
 }
 
+ff_reg_rsq <- function(y, x, df) {
+  # Get the regression formula
+  formula = as.formula(paste(y, " ~ ", 
+                             paste(x, collapse = '+')))
+  # Run regression
+  reg_fit <- lm(formula, df)
+  summary(reg_fit)$r.squared
+}
+
 #' Returns latex/ascii table of regression results
 #' on a data frame where each column takes turns
 #' as the dependent variable, the rest being indep.
@@ -42,15 +51,25 @@ ff_reg <- function(y, x, df) {
 ff_reg_exclude_one <- function(df) {
   factors <- colnames(df)
   
+  # Regressions
   out_list = lapply(seq(length(factors)), function(f) {
     y = factors[f]
     x = factors[-f]
     ff_reg(y, x, df)
   })
   
+  # Rsquareds
+  out_rsq = lapply(seq(length(factors)), function(f) {
+    y = factors[f]
+    x = factors[-f]
+    ff_reg_rsq(y, x, df)
+  })
+  
   names(out_list) = factors
+  names(out_rsq) = factors
   stargazer(out_list, order = c('Constant', factors), column.labels = factors, digits = 4)
   stargazer(out_list, order = c('Constant', factors), type = 'text', column.labels = factors, digits = 4)
+  out_rsq
 }
 
 
@@ -59,3 +78,8 @@ ff_reg_exclude_one <- function(df) {
 df %>% select(Mkt.RF, SMB, HML, CMA, RMW) %>% ff_reg_exclude_one()
 
 df %>% select(Mkt.RF, SMB, HML, CMA, RMW, Mom) %>% ff_reg_exclude_one()
+
+
+# Get rsquared ------------------------------------------------------------
+
+
