05164a9588b899e92b71aea58b85b95fcd04cc52
Victor Andrée <hello@victorandree.se>
2016-11-17 11:48:06 +0100

    updated codez

--- do_correlations_rolling_simulated.R
+++ do_correlations_rolling_simulated.R
@@ -5,17 +5,22 @@ rm(list = ls())
 library(foreach)
 
 MODEL_NAME <- 'std'
-load(sprintf('data/derived/distributions/full_dynamic_%s_10000.RData', MODEL_NAME))
+PATH <- file.path('data/derived/stdresid/dynamic', MODEL_NAME)
+
+distribution <- lapply(list.files(PATH), function(p) {
+  load(file.path(PATH, p))
+  df <- data.frame(stdresid_t)
+  colnames(df) <- c('Mkt.RF','HML','SMB','Mom','RMW','CMA')
+  df
+})
 
 rolling_correlations_simulated <- function(distribution, window) {
-  T_final <- dim(distribution)[3]
+  T_final <- length(distribution)
   
   foreach(t = window:T_final) %do% {
     # Subset the slices and stack them on top of each other
-    subset <- distribution[,, (t - window + 1):t]
-    subset <- bind_rows(
-      lapply(seq(dim(subset)[3]), function(i) data.frame(subset[,, i]))
-    )
+    subset <- distribution[(t - window + 1):t]
+    subset <- bind_rows(subset)
     
     cor(subset)
   }
