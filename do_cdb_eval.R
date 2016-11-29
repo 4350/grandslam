6826ad6a90a4d55d4fc8fe426a22ddb2c05a450e
Gustaf Soldan <gustafsoldan@gmail.com>
2016-11-29 15:34:03 +0100

    cdb SE fixed, and in 100s

--- do_cdb_eval.R
+++ do_cdb_eval.R
@@ -19,7 +19,7 @@ NAME = 'CDB'
 MODEL_NAME <- 'cdb/constrOptim_q5_full_dynamic_std_10000'
 # MV
 #MODEL_NAME <- 'mv/results_full_dynamic_std_10000'
-
+#MODEL_NAME <- 'mv/results_sample'
 
 #  ------------------------------------------------------------------------
 
@@ -27,14 +27,14 @@ MODEL_NAME <- 'cdb/constrOptim_q5_full_dynamic_std_10000'
 factor_models <- list(
   "5F" = list(
     '5F',
-    '5F_EXCL_CMA',
     '5F_EXCL_HML',
+    '5F_EXCL_CMA',
     '5F_EXCL_RMW'
   ),
   "6F" = list(
     '6F',
-    '6F_EXCL_CMA',
     '6F_EXCL_HML',
+    '6F_EXCL_CMA',
     '6F_EXCL_RMW'
   )
 )
@@ -56,7 +56,7 @@ cdb_cdb <- lapply(names(factor_models), function(factor_name) {
     )
   })
   
-  names(factor_cdb) <- c('ALL', 'EXCL_CMA', 'EXCL_HML', 'EXCL_RMW')
+  names(factor_cdb) <- c('ALL', 'EXCL_HML', 'EXCL_CMA', 'EXCL_RMW')
   bind_rows(factor_cdb, .id = 'Strategy')
 })
 names(cdb_cdb) <- c('Five-factor model', 'Six-factor model')
@@ -64,8 +64,8 @@ cdb_cdb <- bind_rows(cdb_cdb, .id = 'Factors')
 
 cdb_cdb$Strategy <- factor(
   cdb_cdb$Strategy,
-  c('ALL', 'EXCL_CMA', 'EXCL_HML', 'EXCL_RMW'),
-  c('All', 'Excl. CMA', 'Excl. HML', 'Excl. RMW')
+  c('ALL', 'EXCL_HML', 'EXCL_CMA', 'EXCL_RMW'),
+  c('All', 'Excl. HML', 'Excl. CMA', 'Excl. RMW')
 )
 
 g <- ggplot(cdb_cdb, aes(x = Week, y = CDB, color = Strategy)) +
@@ -95,40 +95,47 @@ ggsave(
 
 # Means and hypothesis testing ----
 
-# cdb <- lapply(factor_models, function(factor) {
-#   cdb <- lapply(factor, function(strategy) get_cdb(sprintf('%s_%s', MODEL_NAME, strategy))$cdb)
-#   names(cdb) <- factor
-#   cdb
-# })
-# 
-# cdb <- data.frame(cdb)
-# 
-# # H0: X == Y
-# pairs <- list(
-#   list(cdb$X5F.5F, cdb$X5F.5F_EXCL_CMA),
-#   list(cdb$X5F.5F, cdb$X5F.5F_EXCL_HML),
-#   list(cdb$X5F.5F, cdb$X5F.5F_EXCL_RMW),
-#   
-#   
-#   list(cdb$X6F.6F, cdb$X6F.6F_EXCL_CMA),
-#   list(cdb$X6F.6F, cdb$X6F.6F_EXCL_HML),
-#   list(cdb$X6F.6F, cdb$X6F.6F_EXCL_RMW),
-#   # list(cdb$X6F.6F_EXCL_CMA, cdb$X6F.6F_EXCL_HML),
-#   
-#   list(cdb$X5F.5F_EXCL_CMA, cdb$X5F.5F_EXCL_HML),
-#   list(cdb$X5F.6F_EXCL_CMA, cdb$X5F.6F_EXCL_HML)
-#   # list(cdb$X6F.6F, cdb$X5F.5F)
-# )
-# 
-# tested <- bind_rows(lapply(pairs, function(pair) {
-#   test <- t.test(100 * pair[[1]], 100 * pair[[2]], paired = T)
-#   list(
-#     statistic = test$statistic,
-#     estimate = test$estimate,
-#     se = test$estimate / test$statistic,
-#     p = test$p.value
-#   )
-# }))
-# 
-# stargazer::stargazer(100 * cdb, type = 'text', digits = 2)
-# stargazer::stargazer(data.frame(tested), type = 'text', summary = F, digits = 2, digits.extra = 0)
+cdb <- lapply(factor_models, function(factor) {
+  cdb <- lapply(factor, function(strategy) get_cdb(sprintf('%s_%s', MODEL_NAME, strategy))$cdb)
+  names(cdb) <- factor
+  cdb
+})
+
+cdb <- data.frame(cdb)
+
+# H0: X == Y
+pairs <- list(
+  list(cdb$X5F.5F, cdb$X5F.5F_EXCL_HML),
+  list(cdb$X5F.5F, cdb$X5F.5F_EXCL_CMA),
+  list(cdb$X5F.5F, cdb$X5F.5F_EXCL_RMW),
+
+
+  list(cdb$X6F.6F, cdb$X6F.6F_EXCL_HML),
+  list(cdb$X6F.6F, cdb$X6F.6F_EXCL_CMA),
+  list(cdb$X6F.6F, cdb$X6F.6F_EXCL_RMW),
+  # list(cdb$X6F.6F_EXCL_CMA, cdb$X6F.6F_EXCL_HML),
+
+  list(cdb$X5F.5F_EXCL_HML, cdb$X5F.5F_EXCL_CMA),
+  list(cdb$X6F.6F_EXCL_HML, cdb$X6F.6F_EXCL_CMA),
+  # list(cdb$X6F.6F, cdb$X5F.5F)
+  
+  list(cdb$X5F.5F_EXCL_HML, cdb$X5F.5F_EXCL_RMW),
+  list(cdb$X6F.6F_EXCL_HML, cdb$X6F.6F_EXCL_RMW),
+  
+  list(cdb$X5F.5F_EXCL_CMA, cdb$X5F.5F_EXCL_RMW),
+  list(cdb$X6F.6F_EXCL_CMA, cdb$X6F.6F_EXCL_RMW)
+  
+)
+
+tested <- bind_rows(lapply(pairs, function(pair) {
+  test <- t.test(100 * pair[[1]], 100 * pair[[2]], paired = T)
+  list(
+    statistic = test$statistic,
+    estimate = test$estimate,
+    se = test$estimate / test$statistic,
+    p = test$p.value
+  )
+}))
+
+stargazer::stargazer(100 * cdb, type = 'text', digits = 2)
+stargazer::stargazer(data.frame(tested), type = 'text', summary = F, digits = 2, digits.extra = 0)
